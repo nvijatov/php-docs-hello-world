48f0f196c10c481d3905bd31d939cfb4a9d855f3
Nenad Vijatov <nenad.vijatov@gmail.com>
2022-10-30 20:03:56 +0100

    ver7

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <?php
 function get_client_ip()
 {
-    foreach (array(
-                'HTTP_CLIENT_IP',
-                'HTTP_X_FORWARDED_FOR',
-                'HTTP_X_FORWARDED',
-                'HTTP_X_CLUSTER_CLIENT_IP',
-                'HTTP_FORWARDED_FOR',
-                'HTTP_FORWARDED',
-                'REMOTE_ADDR') as $key) {
-        if (array_key_exists($key, $_SERVER)) {
-            foreach (explode(',', $_SERVER[$key]) as $ip) {
-                $ip = trim($ip);
-                if ((bool) filter_var($ip, FILTER_VALIDATE_IP,
-                                FILTER_FLAG_IPV4 |
-                                FILTER_FLAG_NO_PRIV_RANGE |
-                                FILTER_FLAG_NO_RES_RANGE)) {
-                    return $ip;
-                }
-            }
-        }
-    }
-    return null;
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
 }
 
-echo 'Hello! Your IP addres is: ' . get_client_ip();
+$ip_addr = get_client_ip();
 
+echo 'Hello! Your IP addres is: ' . $ip_addr;
 ?>
